798934139662f66280311d6cefbfd415c79f497f
Rafa Cabeza <rafacabeza@gmail.com>
2018-09-12 13:15:45 +0200

    solucion suma

--- sumaFiles.js
+++ sumaFiles.js
@@ -1,4 +1,25 @@
 const fs = require('fs')
-const numero1 = fs.readFileSync('./numero1', 'utf-8')
-const numero2 = fs.readFileSync('./numero2', 'utf-8')
-console.log(`El resultado de la suma es  ${parseInt(numero1)+parseInt(numero2)}`)
+var numero1
+
+const getData = (fileName, type) =>
+
+new Promise((resolve, reject) => {
+  fs.readFile(fileName, type, (err, data) => {
+    err ? reject(err) : resolve(parseInt(data))
+  })
+})
+
+var promise1 = getData('numero1', 'utf-8')
+var promise2 = getData('numero2', 'utf-8')
+Promise.all([promise1, promise2]).
+then((arrayValues) => {
+  console.log('El numero 1 es ' + arrayValues[0])
+  let suma = 0;
+  arrayValues.forEach(element => {
+    suma += element
+  });
+  console.log(suma)
+  //con la funcion reduce
+  let sum = arrayValues.reduce((sum, x) => sum + x)
+  console.log(sum)
+})
